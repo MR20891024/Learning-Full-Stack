d0b1e00db948bab81f93cace4d3f8f49a0c6aba7
Mahdi Rahimi <mahdi.rahimi@agriculture.vic.gov.au>
2023-07-14 12:15:04 +1000

    add some event listeners

---
+++ basicPage/code.js
@@ -0,0 +1,11 @@
+const baba = () => {
+    console.log('Ha nafte baba !!!');
+}
+
+const Aghesh = (e) => {
+    console.log(e)
+}
+
+const btn = document.querySelector('#greating-btn');
+
+btn.addEventListener('click', baba);
